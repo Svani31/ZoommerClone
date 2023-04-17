fbb3e877d5bd8fc5cd4c853f2db3ecfeaf84ee96
Giorgi Ratiani <gembani0@gmail.com>
2023-04-17 21:51:25 +0400

    creating firebase and registration

--- src/Components/header-component/link-component/cart-item-component/cart-item.tsx
+++ src/Components/header-component/link-component/cart-item-component/cart-item.tsx
@@ -1,8 +1,24 @@
+import "./cart-item.scss"
 
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from "react-router-dom";
 
 const CartItem = () =>{
     return(
-            <h2>CartItem</h2>
+            <div className='header__cartitem'>
+                <Link className="cartitem__link" to={"/cart"}>
+                    <ShoppingCartOutlinedIcon/>
+                    <span>0 ლ</span>
+                </Link>
+                <div className="cartitem__dropdown">
+                    <div className="cartitem__inner">
+                    <h5>კალათა</h5>
+                    <h4>ჯამი : <span>0 ₾</span></h4>
+                    <button>კალათაში გადასვლა</button>
+                    </div>
+                </div>
+            </div>
+
     )
 }
 
